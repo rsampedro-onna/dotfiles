17be1fc07d4c7885be2031618904a3f517136c1d
rafaelnonato <rafaeln@gmail.com>
2017-05-09 11:34:41 -0400

    no keybind for changing keyboard layout. click on i3bar instead

--- .i3/layout.sh
+++ .i3/layout.sh
@@ -10,6 +10,8 @@
 ####################################################
 
 case $BLOCK_BUTTON in
+    1) setxkbmap -layout us -variant intl;; # left button
+    3) setxkbmap -layout br -variant dvorak-altdeadkeys;; # right button
     4) xkblayout-state set -1;;  # wheel up
     5) xkblayout-state set +1;;  # wheel down
 esac
