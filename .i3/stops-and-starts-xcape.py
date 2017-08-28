f5dc2e87eac970a285c195b8768821ce11641a23
rafaelnonato <rafaeln@gmail.com>
2017-08-27 20:01:47 -0500

    now i can use xcape in xwrits control window

--- .i3/stops-and-starts-xcape.py
+++ .i3/stops-and-starts-xcape.py
@@ -7,8 +7,8 @@ i3 = i3ipc.Connection()
 
 def on_window_focus(i3, e):
     focused = i3.get_tree().find_focused()
-    name = focused.window_class
-    if name == "VirtualBox":
+    name = focused.name
+    if name.startswith("7"):
         subprocess.call(["killall", "xcape"])
     else:
         subprocess.call(["killall", "xcape"])
