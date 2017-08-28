92654d4286a21dfe004e5197c4054766be399b15
rafaelnonato <rafaeln@gmail.com>
2017-08-28 12:10:05 -0500

    Improvements to i3 plugins

--- .i3/stops-and-starts-xcape.py
+++ .i3/stops-and-starts-xcape.py
@@ -8,7 +8,7 @@ i3 = i3ipc.Connection()
 def on_window_focus(i3, e):
     focused = i3.get_tree().find_focused()
     name = focused.name
-    if name.startswith("7"):
+    if name.startswith("7") and name.endswith('VirtualBox'):
         subprocess.call(["killall", "xcape"])
     else:
         subprocess.call(["killall", "xcape"])
